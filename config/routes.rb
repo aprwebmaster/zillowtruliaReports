e8c2790a409882a6aca4da7829ab53311e99b469
aprwebmaster <cbradford@apr.com>
2018-01-10 15:15:56 -0800

    testing hoodlines embed

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,8 @@ Rails.application.routes.draw do
 
   get 'welcome/forclosures'
 
+  get 'welcome/hoodlinetest'
+
   root to: 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
